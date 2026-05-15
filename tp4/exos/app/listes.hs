14a94c3577c5bfcb90d1a848bd8e506175bbd3a7
Julien Dehos <dehos@univ-littoral.fr>
2026-05-15 12:58:37 +0200

    tp4, exos

---
+++ tp4/exos/app/listes.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import Data.Char ( toUpper )
+
+-- TODO mylength
+
+-- TODO mylength'
+
+-- TODO toUpperString
+
+-- TODO onlyLetters
+
+-- TODO mulListe
+
+-- TODO selectListe
+
+-- TODO sumListe
+
+-- TODO mylast
+
+-- TODO myinit
+
+-- TODO myreplicate
+
+-- TODO mydrop
+
+-- TODO mytake
+
+main :: IO ()
+main = do
+    putStrLn "listes"
---
+++ tp4/exos/app/recursivite.hs
@@ -0,0 +1,16 @@
+
+module Main where
+
+import System.IO ( hFlush, stdout )
+
+-- TODO pgcd :: Integer -> Integer -> Integer
+
+-- TODO fact :: Integer -> Integer
+
+-- TODO loopEcho :: Int -> IO 
+
+main :: IO ()
+main = do
+    putStrLn "recursivite"
+    -- TODO
+    
