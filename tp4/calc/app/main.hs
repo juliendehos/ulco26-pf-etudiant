45da3fd3affb7d38ac8f8c577258a4d383113c05
Julien Dehos <dehos@univ-littoral.fr>
2026-05-18 14:47:29 +0200

    tp4, calc

---
+++ tp4/calc/app/main.hs
@@ -0,0 +1,15 @@
+
+import System.IO ( hFlush, stdout )
+
+--
+-- retourne le resultat et les tokens non consommés
+calc :: [String] -> (Integer, [String])
+calc tokens = error "not implemented"
+-- TODO
+
+main :: IO ()
+main = do
+    putStr "> "
+    hFlush stdout
+    -- TODO
+
